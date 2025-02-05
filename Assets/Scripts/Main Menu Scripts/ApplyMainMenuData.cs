bc6f2634f4d5ac51b2508fe207fdc8cb1a008233
iliketurtles14 <creeperdude81@gmail.com>
2025-02-04 23:27:03 -0500

    2/4/2025

--- Assets/Scripts/Main Menu Scripts/ApplyMainMenuData.cs
+++ Assets/Scripts/Main Menu Scripts/ApplyMainMenuData.cs
@@ -49,11 +49,11 @@ public class ApplyMainMenuData : MonoBehaviour
         //mouse
         mmc.Find("MouseOverlay").GetComponent<Image>().sprite = UISprites[41];
         //main menu buttons
-        mmc.Find("PlayButton").GetComponent<Image>().sprite = UISprites[198];
-        mmc.Find("OptionsButton").GetComponent<Image>().sprite = UISprites[198];
-        mmc.Find("MapEditorButton").GetComponent<Image>().sprite = UISprites[198];
+        mmc.Find("TitlePanel").Find("PlayButton").GetComponent<Image>().sprite = UISprites[198];
+        mmc.Find("TitlePanel").Find("OptionsButton").GetComponent<Image>().sprite = UISprites[198];
+        mmc.Find("TitlePanel").Find("MapEditorButton").GetComponent<Image>().sprite = UISprites[198];
         //escapists
-        mmc.Find("The Escapists").GetComponent<Image>().sprite = UISprites[222];
+        mmc.Find("TitlePanel").Find("The Escapists").GetComponent<Image>().sprite = UISprites[222];
         //prison select backdrop
         mmc.Find("PrisonSelectPanel").GetComponent<Image>().sprite = UISprites[208];
         //arrows
@@ -62,7 +62,40 @@ public class ApplyMainMenuData : MonoBehaviour
         //prison select buttons
         mmc.Find("PrisonSelectPanel").Find("BackButton").GetComponent<Image>().sprite = UISprites[248];
         mmc.Find("PrisonSelectPanel").Find("ContinueButton").GetComponent<Image>().sprite = UISprites[248];
-        
+        //prison image
+        mmc.Find("PrisonSelectPanel").Find("PrisonImage").GetComponent<Image>().sprite = UISprites[519];
+        //player menu buttons
+        mmc.Find("PlayerPanel").Find("BackButton").GetComponent<Image>().sprite = UISprites[248];
+        mmc.Find("PlayerPanel").Find("ContinueButton").GetComponent<Image>().sprite = UISprites[248];
+        //arrows
+        mmc.Find("PlayerPanel").Find("LeftArrow").GetComponent<Image>().sprite = UISprites[203];
+        mmc.Find("PlayerPanel").Find("RightArrow").GetComponent<Image>().sprite = UISprites[200];
+        //player menu backdrop
+        mmc.Find("PlayerPanel").GetComponent<Image>().sprite = UISprites[184];
+        //player backdrop
+        mmc.Find("PlayerPanel").Find("NPCBackdrop").GetComponent<Image>().sprite = UISprites[255];
+        //name box
+        mmc.Find("PlayerPanel").Find("NameBox").GetComponent<Image>().sprite = UISprites[186];
+        //small menu panel set button
+        mmc.Find("SmallMenuPanel").Find("SetButton").GetComponent<Image>().sprite = UISprites[326];
+        //arros
+        mmc.Find("SmallMenuPanel").Find("LeftArrow").GetComponent<Image>().sprite = UISprites[203];
+        mmc.Find("SmallMenuPanel").Find("RightArrow").GetComponent<Image>().sprite = UISprites[200];
+        //npc backdrop
+        mmc.Find("SmallMenuPanel").Find("NPCBackdrop").GetComponent<Image>().sprite = UISprites[255];
+        //name box
+        mmc.Find("SmallMenuPanel").Find("NameBox").GetComponent<Image>().sprite = UISprites[186];
+        //npc customize panel backdrop
+        mmc.Find("NPCCustomizePanel").GetComponent<Image>().sprite = UISprites[320];
+        //npc backdrop
+        mmc.Find("NPCCustomizePanel").Find("NPCBackdrop").GetComponent<Image>().sprite = UISprites[321];
+        //name box
+        mmc.Find("NPCCustomizePanel").Find("NameBox").GetComponent<Image>().sprite = UISprites[322];
+        //npc customize panel buttons
+        mmc.Find("NPCCustomizePanel").Find("BackButton").GetComponent<Image>().sprite = UISprites[326];
+        mmc.Find("NPCCustomizePanel").Find("RandomButton").GetComponent<Image>().sprite = UISprites[330];
+        mmc.Find("NPCCustomizePanel").Find("StartGameButton").GetComponent<Image>().sprite = UISprites[328];
+
     }
 
 }
