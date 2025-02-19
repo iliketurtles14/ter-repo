60a972d8e434c342eed60e2d0bb298b8ef1a3ba8
iliketurtles14 <creeperdude81@gmail.com>
2025-02-18 23:34:30 -0500

    2/18/2025

--- Assets/Scripts/ApplyPrisonData.cs
+++ Assets/Scripts/ApplyPrisonData.cs
@@ -17,6 +17,17 @@ public class ApplyPrisonData : MonoBehaviour
     private List<Sprite> PrisonObjectSprites = new List<Sprite>();
     private List<Sprite> UISprites = new List<Sprite>();
 
+    public List<Sprite> RabbitHoldingSprites = new List<Sprite>();
+    public List<Sprite> BaldEagleHoldingSprites = new List<Sprite>();
+    public List<Sprite> LiferHoldingSprites = new List<Sprite>();
+    public List<Sprite> YoungBuckHoldingSprites = new List<Sprite>();
+    public List<Sprite> OldTimerHoldingSprites = new List<Sprite>();
+    public List<Sprite> BillyGoatHoldingSprites = new List<Sprite>();
+    public List<Sprite> FrosephHoldingSprites = new List<Sprite>();
+    public List<Sprite> TangoHoldingSprites = new List<Sprite>();
+    public List<Sprite> MaruHoldingSprites = new List<Sprite>();
+    public List<Sprite> InmateOutfitHoldingSprites = new List<Sprite>();
+
     private void Start()
     {
         senderScript = DataSender.instance;
@@ -66,5 +77,86 @@ public class ApplyPrisonData : MonoBehaviour
         Resources.Load("BarLine").GetComponent<Image>().sprite = UISprites[44];
         //desk (to be removed later)
         PlayerDesk.GetComponent<SpriteRenderer>().sprite = PrisonObjectSprites[143];
+        //holding sprites
+        RabbitHoldingSprites.Add(NPCSprites[127]);
+        RabbitHoldingSprites.Add(NPCSprites[128]);
+        RabbitHoldingSprites.Add(NPCSprites[133]);
+        RabbitHoldingSprites.Add(NPCSprites[134]);
+        RabbitHoldingSprites.Add(NPCSprites[129]);
+        RabbitHoldingSprites.Add(NPCSprites[130]);
+        RabbitHoldingSprites.Add(NPCSprites[131]);
+        RabbitHoldingSprites.Add(NPCSprites[132]);
+        BaldEagleHoldingSprites.Add(NPCSprites[404]);
+        BaldEagleHoldingSprites.Add(NPCSprites[405]);
+        BaldEagleHoldingSprites.Add(NPCSprites[410]);
+        BaldEagleHoldingSprites.Add(NPCSprites[411]);
+        BaldEagleHoldingSprites.Add(NPCSprites[406]);
+        BaldEagleHoldingSprites.Add(NPCSprites[407]);
+        BaldEagleHoldingSprites.Add(NPCSprites[409]);
+        BaldEagleHoldingSprites.Add(NPCSprites[408]);
+        LiferHoldingSprites.Add(NPCSprites[444]);
+        LiferHoldingSprites.Add(NPCSprites[445]);
+        LiferHoldingSprites.Add(NPCSprites[450]);
+        LiferHoldingSprites.Add(NPCSprites[451]);
+        LiferHoldingSprites.Add(NPCSprites[446]);
+        LiferHoldingSprites.Add(NPCSprites[447]);
+        LiferHoldingSprites.Add(NPCSprites[449]);
+        LiferHoldingSprites.Add(NPCSprites[448]);
+        YoungBuckHoldingSprites.Add(NPCSprites[412]);
+        YoungBuckHoldingSprites.Add(NPCSprites[413]);
+        YoungBuckHoldingSprites.Add(NPCSprites[416]);
+        YoungBuckHoldingSprites.Add(NPCSprites[417]);
+        YoungBuckHoldingSprites.Add(NPCSprites[415]);
+        YoungBuckHoldingSprites.Add(NPCSprites[414]);
+        YoungBuckHoldingSprites.Add(NPCSprites[419]);
+        YoungBuckHoldingSprites.Add(NPCSprites[418]);
+        OldTimerHoldingSprites.Add(NPCSprites[436]);
+        OldTimerHoldingSprites.Add(NPCSprites[437]);
+        OldTimerHoldingSprites.Add(NPCSprites[442]);
+        OldTimerHoldingSprites.Add(NPCSprites[443]);
+        OldTimerHoldingSprites.Add(NPCSprites[438]);
+        OldTimerHoldingSprites.Add(NPCSprites[439]);
+        OldTimerHoldingSprites.Add(NPCSprites[441]);
+        OldTimerHoldingSprites.Add(NPCSprites[442]);
+        BillyGoatHoldingSprites.Add(NPCSprites[428]);
+        BillyGoatHoldingSprites.Add(NPCSprites[429]);
+        BillyGoatHoldingSprites.Add(NPCSprites[432]);
+        BillyGoatHoldingSprites.Add(NPCSprites[433]);
+        BillyGoatHoldingSprites.Add(NPCSprites[430]);
+        BillyGoatHoldingSprites.Add(NPCSprites[431]);
+        BillyGoatHoldingSprites.Add(NPCSprites[435]);
+        BillyGoatHoldingSprites.Add(NPCSprites[434]);
+        FrosephHoldingSprites.Add(NPCSprites[455]);
+        FrosephHoldingSprites.Add(NPCSprites[454]);
+        FrosephHoldingSprites.Add(NPCSprites[458]);
+        FrosephHoldingSprites.Add(NPCSprites[459]);
+        FrosephHoldingSprites.Add(NPCSprites[456]);
+        FrosephHoldingSprites.Add(NPCSprites[457]);
+        FrosephHoldingSprites.Add(NPCSprites[453]);
+        FrosephHoldingSprites.Add(NPCSprites[452]);
+        TangoHoldingSprites.Add(NPCSprites[420]);
+        TangoHoldingSprites.Add(NPCSprites[421]);
+        TangoHoldingSprites.Add(NPCSprites[424]);
+        TangoHoldingSprites.Add(NPCSprites[425]);
+        TangoHoldingSprites.Add(NPCSprites[423]);
+        TangoHoldingSprites.Add(NPCSprites[422]);
+        TangoHoldingSprites.Add(NPCSprites[427]);
+        TangoHoldingSprites.Add(NPCSprites[426]);
+        MaruHoldingSprites.Add(NPCSprites[1283]);
+        MaruHoldingSprites.Add(NPCSprites[1284]);
+        MaruHoldingSprites.Add(NPCSprites[1287]);
+        MaruHoldingSprites.Add(NPCSprites[1288]);
+        MaruHoldingSprites.Add(NPCSprites[1281]);
+        MaruHoldingSprites.Add(NPCSprites[1282]);
+        MaruHoldingSprites.Add(NPCSprites[1286]);
+        MaruHoldingSprites.Add(NPCSprites[1285]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[122]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[121]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[125]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[126]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[123]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[124]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[120]);
+        InmateOutfitHoldingSprites.Add(NPCSprites[119]);
     }
 }
