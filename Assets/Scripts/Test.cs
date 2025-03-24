6b5e02d27bb572721f15683357414aa285e4f56a
iliketurtles14 <creeperdude81@gmail.com>
2025-03-23 22:14:50 -0400

    3/23/2025

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            //Resources.Load<Texture2D>("Center Perks Tileset/tile000") = DataSender.instance.GetComponent<DataSender>().TileList[96].texture;
+        }
+    }
+}
