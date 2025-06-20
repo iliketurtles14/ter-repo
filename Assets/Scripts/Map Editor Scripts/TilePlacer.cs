7d971ecb91c930eeaa6d0e9a6c427860a600ee19
iliketurtles14 <creeperdude81@gmail.com>
2025-06-20 01:06:39 -0400

    6/19/2025

--- Assets/Scripts/Map Editor Scripts/TilePlacer.cs
+++ Assets/Scripts/Map Editor Scripts/TilePlacer.cs
@@ -7,14 +7,43 @@ public class TilePlacer : MonoBehaviour
     public MouseCollisionOnMap mcs;
     public Transform tiles;
     public bool canPlace;
+    public LayerController layerControllerScript;
+    private bool inDeleteMode = false;
+    private string layer;
+    private void Start()
+    {
+        foreach(Transform layer in tiles)
+        {
+            GameObject empty = new GameObject();
+            empty.name = "empty";
+            empty.transform.position = new Vector3(9999, 9999);
+            empty.transform.SetParent(layer);
+        }
+    }
     private void Update()
     {
-        if(highlight.position != new Vector3(9999, 9999) && !mcs.isTouchingMenu && !mcs.isTouchingButton && Input.GetMouseButton(0) && GetComponent<TileSelect>().hasSelected)
+        if(!inDeleteMode && highlight.position != new Vector3(9999, 9999) && !mcs.isTouchingMenu && !mcs.isTouchingButton && Input.GetMouseButton(0) && GetComponent<TileSelect>().hasSelected)
         {
-            Debug.Log("In if");
+            switch (layerControllerScript.currentLayer)
+            {
+                case 0:
+                    layer = "Underground";
+                    break;
+                case 1:
+                    layer = "Ground";
+                    break;
+                case 2:
+                    layer = "Vent";
+                    break;
+                case 3:
+                    layer = "Roof";
+                    break;
+                case 4:
+                    layer = "Zones";
+                    break;
+            }
 
-            //change this for when you have all layers
-            foreach(Transform tile in tiles.Find("Ground"))
+            foreach(Transform tile in tiles.Find(layer))
             {
                 if(tile.position == highlight.position)
                 {
@@ -29,8 +58,32 @@ public class TilePlacer : MonoBehaviour
 
             if (canPlace)
             {
-                Debug.Log("Placing Tile");
-                PlaceTile("Ground", GetComponent<TileSelect>().selectedTile);
+                PlaceTile(layer, GetComponent<TileSelect>().selectedTile);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !inDeleteMode)
+        {
+            inDeleteMode = true;
+
+            highlight.GetComponent<SpriteRenderer>().color = new Color(255f / 255f, 0, 0, 150f / 255f);
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && inDeleteMode)
+        {
+            inDeleteMode = false;
+
+            highlight.GetComponent<SpriteRenderer>().color = new Color(255f / 255f, 220f / 255f, 0, 150f / 255f);
+        }
+
+        if(highlight.position != new Vector3(9999, 9999) && !mcs.isTouchingMenu && !mcs.isTouchingButton && Input.GetMouseButton(0) && inDeleteMode)
+        {
+            foreach(Transform tile in tiles.Find(layer))
+            {
+                if(tile.position == highlight.position)
+                {
+                    Destroy(tile.gameObject);
+                    break;
+                }
             }
         }
     }
@@ -41,10 +94,11 @@ public class TilePlacer : MonoBehaviour
         placedTile.AddComponent<BoxCollider2D>();
         placedTile.name = tileToPlace.name;
         placedTile.transform.position = highlight.position;
+        placedTile.transform.SetParent(tiles.Find(layer));
         placedTile.GetComponent<BoxCollider2D>().size = new Vector2(1.6f, 1.6f);
         placedTile.GetComponent<SpriteRenderer>().sprite = tileToPlace.GetComponent<Image>().sprite;
         placedTile.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Sliced;
         placedTile.GetComponent<SpriteRenderer>().size = new Vector2(1.6f, 1.6f);
-        Instantiate(placedTile, tiles.Find(layer));
+        placedTile.GetComponent<SpriteRenderer>().sortingOrder = 1;
     }
 }
